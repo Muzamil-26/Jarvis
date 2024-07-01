d740858c4c9327e074cbef77e9aa5b3bb4d4a7b0
Muzamil-26 <155745476+Muzamil-26@users.noreply.github.com>
2024-07-01 18:54:25 +0530

    Create songsoropenreq.py

---
+++ songsoropenreq.py
@@ -0,0 +1,13 @@
+songs={
+    "doraemon":"https://www.youtube.com/watch?v=T38K7q1IhEQ&pp=ygUYZG9yZWFtb24gb2xkIGVwIGluIGhpbmRp",
+    "sajni":"https://www.youtube.com/watch?v=k3g_WjLCsXM&pp=ygUSYXJpaml0IHNpbmdoIHNvbmdz",
+    "google":"https://www.google.com",
+    "youtube":"https://www.youtube.com",
+    "instagram":"https://www.instagram.com",
+    "gitub":"https://github.com",
+    "linkedin":"https://www.linkedin.com"
+}
+# opens={
+#     "play google":f"webbrowser.open('https://www.google.com')",
+
+# }
